b6875a0c330499e82611e1110120f4e974c7ae50
user <yenje7@naver.com>
2023-02-12 00:37:07 +0900

    데이터 조회 기능

--- src/main/java/com/example/BoardProject/entity/Article.java
+++ src/main/java/com/example/BoardProject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.BoardProject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Article {
     @Id // 대표값 지정
