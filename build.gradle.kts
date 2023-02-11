e866ac972a260185013a884c6da6531e031981a2
user <yenje7@naver.com>
2023-02-11 22:25:34 +0900

    h2 데이터베이스 및 롬복 사용

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,16 @@ repositories {
 }
 
 dependencies {
+	// 롬복 추가
+	compileOnly ("org.projectlombok:lombok")
+	annotationProcessor ("org.projectlombok:lombok")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<Test> {
