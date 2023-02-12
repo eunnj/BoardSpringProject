b463502f0a67a38269c60ea4c4ecabade794959c
user <yenje7@naver.com>
2023-02-12 11:08:11 +0900

    데이터 전체 조회

--- src/main/java/com/example/BoardProject/repository/ArticleRepository.java
+++ src/main/java/com/example/BoardProject/repository/ArticleRepository.java
@@ -3,7 +3,10 @@ package com.example.BoardProject.repository;
 import com.example.BoardProject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 //  <관리 대상, 기본 값의 타입 >
 public interface ArticleRepository extends CrudRepository<Article,Long> {
-
+    @Override
+    ArrayList<Article> findAll();
 }
