2e848ef6c1456ff64e894cf5692f81c39f054a33
user <yenje7@naver.com>
2023-02-19 16:33:28 +0900

    로깅 설정 수정

--- src/main/java/com/example/BoardProject/entity/Article.java
+++ src/main/java/com/example/BoardProject/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity // DB가 해당 객체를 인식 가능!
@@ -18,7 +15,7 @@ import javax.persistence.Id;
 @Getter
 public class Article {
     @Id // 대표값 지정
-    @GeneratedValue // 1,2,3 ... 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id 자동 생성 어노테이션
     private Long id;
 
     @Column
