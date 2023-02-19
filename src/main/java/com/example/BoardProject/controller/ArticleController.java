92e30c299cf5e697475887a5466b1e8193847a3f
user <yenje7@naver.com>
2023-02-19 16:12:14 +0900

    게시글 삭제하기

--- src/main/java/com/example/BoardProject/controller/ArticleController.java
+++ src/main/java/com/example/BoardProject/controller/ArticleController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -101,5 +103,24 @@ public class ArticleController {
 
         // 3. 수정 결과 페이지로 리다이렉트
         return "redirect:/articles/"+articleEntity.getId();
+    }
+
+    // html에서 delete 요청을 공식적으로 지원하지 않음
+    // 따라서 GetMapping으로 처리
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제요청");
+
+        // 1. 삭제 대상 가져오기
+        Article target =articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        // 2. 대상 삭제
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제가 완료되었습니다!");
+        }
+
+        // 3. 결과 페이지 리다이렉트
+        return "redirect:/articles";
     }
 }
